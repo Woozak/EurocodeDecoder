4bd8ab200f7934c08bf4016bad99faccc201c5a4
Wuzak <wuzak7@gmail.com>
2024-10-02 15:15:41 +0300

    add decode_eurocode function

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 from re import fullmatch, IGNORECASE
 
+import exceptions
+
 
 def is_eurocode_correct(eurocode: str) -> bool:
     pattern = r'[0-9A-Z]{4}[ABCDEFHMLRT][A-Z]{2}[0-9A-Z]{,8}'
     result = fullmatch(pattern, eurocode, flags=IGNORECASE)
     return bool(result)
 
+
+def decode_eurocode(eurocode: str):
+    if not is_eurocode_correct(eurocode):
+        raise exceptions.InvalidEurocodeError

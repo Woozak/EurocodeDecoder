47761c9c097905d89447286f41f24ad8069d6fde
Wuzak <wuzak7@gmail.com>
2024-10-09 21:17:37 +0300

    refactor, complete class implementation

--- glass_schemas.py
+++ glass_schemas.py
@@ -7,21 +7,14 @@ from glass_codes import windshield, rear_window, side_window
 
 class Glass:
     def __init__(self, eurocode: str):
-        self.is_eurocode_correct(eurocode)
-
         self.eurocode = eurocode
         self.data = self.get_data(eurocode[4])
+        self.is_eurocode_correct(self.data.pattern, eurocode)
+
         self.car_model, self.car_years = self.get_car_model(eurocode[:4])
         self.glass_type = self.data.type[eurocode[4]]
-        self.color = self.data.color[eurocode[5:7]]
-        self.modifications = self.get_modifications(eurocode[-2:])
-
-    @staticmethod
-    def is_eurocode_correct(eurocode: str):
-        pattern = r'[0-9A-Z]{4}[ABCDEFHMLRT][A-Z]{2}[1-9A-Z]{,8}'
-        result = fullmatch(pattern, eurocode)
-        if not result:
-            raise exceptions.InvalidEurocodeError
+        self.color = self.get_attribute(self.data.color, eurocode[5:7])
+        self.modifications = self.get_modifications(self.data.modification, eurocode[-2:])
 
     @staticmethod
     def get_data(code: str):
@@ -33,7 +26,13 @@ class Glass:
             return side_window
 
     @staticmethod
-    def get_car_model(code: str):
+    def is_eurocode_correct(pattern: str, eurocode: str) -> None:
+        result = fullmatch(pattern, eurocode)
+        if not result:
+            raise exceptions.InvalidEurocodeError
+
+    @staticmethod
+    def get_car_model(code: str) -> tuple[str, str]:
         with open('glass_codes/car_models.json', 'r', encoding='utf-8') as file:
             car_models = json.load(file)
             try:
@@ -42,35 +41,54 @@ class Glass:
             except KeyError:
                 raise exceptions.EurocodeNotFoundError
 
-    def get_characteristics(self, code: str):
+    @staticmethod
+    def get_attribute(data, code: str) -> str:
+        try:
+            return data[code]
+        except KeyError:
+            raise exceptions.InvalidEurocodeError
+
+    @staticmethod
+    def get_characteristics(data, code: str) -> list[str]:
         characteristics = []
         pattern = r'[^1-9]*'
         string = search(pattern, code).group()
+        if len(string) != len(set(string)):
+            raise exceptions.InvalidEurocodeError
         for char in string:
-            characteristics.append(self.data.characteristic[char])
+            try:
+                characteristics.append(data.characteristic[char])
+            except KeyError:
+                raise exceptions.InvalidEurocodeError
         return characteristics
 
-    def get_modifications(self, code: str):
-        for mod in self.data.modification:
-            return self.data.modification[mod] if code in mod else None
+    @staticmethod
+    def get_modifications(data, code: str) -> str | None:
+        for mod in data.modification:
+            if code in mod:
+                return data.modification[mod]
 
 
 class Windshield(Glass):
     def __init__(self, eurocode: str):
         super().__init__(eurocode)
         self.strip_color = self.data.strip_color[eurocode[7:9]] if eurocode[7:9] in self.data.strip_color else None
-        self.characteristics = self.get_characteristics(eurocode[9:] if self.strip_color else eurocode[7:])
+        self.characteristics = self.get_characteristics(
+            self.data.characteristic,
+            eurocode[9:] if self.strip_color else eurocode[7:]
+        )
 
 
 class RearWindow(Glass):
     def __init__(self, eurocode: str):
         super().__init__(eurocode)
         self.body_type = self.data.body_type[eurocode[7]]
+        self.characteristics = self.get_characteristics(self.data.characteristic, eurocode[8:])
 
 
 class SideWindow(Glass):
     def __init__(self, eurocode: str):
         super().__init__(eurocode)
-        self.body_type = self.data.body_type[eurocode[7:9]]
-        self.position = self.data.position[eurocode[9:11]]
-        self.characteristics = self.get_characteristics(eurocode[11:])
+        self.body_type = self.get_attribute(self.data.body_type, eurocode[7:9])
+        self.position = self.get_attribute(self.data.position, eurocode[9:11])
+        self.characteristics = self.get_characteristics(self.data.characteristic, eurocode[11:])

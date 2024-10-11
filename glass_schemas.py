44b1ebbcbfde1bf3fb0169b60740aff179fa6553
Wuzak <wuzak7@gmail.com>
2024-10-11 21:15:08 +0300

    remove unnecessary

--- glass_schemas.py
+++ glass_schemas.py
@@ -92,8 +92,3 @@ class SideWindow(Glass):
         self.body_type = self.get_attribute(self.data.body_type, eurocode[7:9])
         self.position = self.get_attribute(self.data.position, eurocode[9:11])
         self.characteristic = self.get_characteristic(self.data.characteristic, eurocode[11:])
-
-
-glass = Windshield('3566AGNPV')
-
-print(glass.__dict__)

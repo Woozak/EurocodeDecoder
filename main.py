2db3ef537fd06bfc8c09f681bcffdd5c65faa6b0
Wuzak <wuzak7@gmail.com>
2024-10-12 14:03:28 +0300

    add /decode endpoint

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, status
 
+from utils import get_glass_info
+from glass_schemas import ResponseGlass
+
 
 app = FastAPI()
 
 
-@app.get('/decode/{eurocode}', status_code=status.HTTP_200_OK)
-def decode_eurocode():
-    pass
+@app.get(
+    '/decode/{eurocode}',
+    response_model=ResponseGlass,
+    response_model_exclude_defaults=True,
+    status_code=status.HTTP_200_OK
+)
+def decode(eurocode: str):
+    return get_glass_info(eurocode)

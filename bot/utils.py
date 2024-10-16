a4430f8b796c91fba6e50b55d54e488d4b3b365a
Wuzak <wuzak7@gmail.com>
2024-10-16 16:16:41 +0300

    add error handling

--- bot/utils.py
+++ bot/utils.py
@@ -4,23 +4,33 @@ import json
 
 def get_eurocode_data(eurocode: str) -> dict:
     url = f'http://127.0.0.1:8000/decode/{eurocode}'
-    data = requests.get(url).json()
-    return data
-
-
-def decode_eurocode(eurocode: str):
-    data = get_eurocode_data(eurocode)
-    with open('translation.json', 'r', encoding='utf-8') as file:
-        translation = json.load(file)
-
-    glass_info = []
-    for key, value in data.items():
-        if key == 'eurocode':
-            continue
-        elif key == 'characteristic':
-            string = f'{translation[key]}: <b>{', '.join(value).capitalize()}</b>'
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError:
+        if response.status_code == 400:
+            raise ValueError('Некорректный еврокод')
+        elif response.status_code == 404:
+            raise ValueError('Еврокод не найден')
         else:
-            string = f'{translation[key]}: <b>{value}</b>'
-        glass_info.append(string)
+            raise ValueError('Произошла ошибка, попробуйте снова')
 
-    return '\n'.join(glass_info)
+
+def decode_eurocode(eurocode: str) -> str:
+    try:
+        data = get_eurocode_data(eurocode)
+        with open('translation.json', 'r', encoding='utf-8') as file:
+            translation = json.load(file)
+        glass_info = []
+        for key, value in data.items():
+            if key == 'eurocode':
+                continue
+            elif key == 'characteristic':
+                string = f'{translation[key]}: <b>{', '.join(value).capitalize()}</b>'
+            else:
+                string = f'{translation[key]}: <b>{value}</b>'
+            glass_info.append(string)
+        return '\n'.join(glass_info)
+    except ValueError as error:
+        return str(error)
